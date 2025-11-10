77ee96749d4cbe7e163be99d6a7d672e9ff7c49e
Nick <javokhirru03@gmail.com>
2025-11-10 17:46:07 +0900

    feat: create routing system

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
+import theme from "./app/MaterialTheme";
 import "./css/index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -12,7 +16,12 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
