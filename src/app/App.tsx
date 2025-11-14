dc26c9c74875044cfd267de0648c3f55a84546ce
Nick <javokhirru03@gmail.com>
2025-11-14 16:40:55 +0900

    feat: integration head and footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import "../css/app.css";
+
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
-import { HomePage } from "./screens/homePage";
-import { UsersPage } from "./screens/usersPage";
-import { OrdersPage } from "./screens/ordersPage";
-import { ProductsPage } from "./screens/productsPage";
-import { HelpPage } from "./screens/helpPage";
-import { HomeNavbar } from "./components/headers/HomeNavbar";
-import { OtherNavbar } from "./components/headers/OtherNavbar";
-import { Footer } from "./components/footer";
+import HomePage from "./screens/homePage";
+import UsersPage from "./screens/usersPage";
+import OrdersPage from "./screens/ordersPage";
+import ProductsPage from "./screens/productsPage";
+import HelpPage from "./screens/helpPage";
+import HomeNavbar from "./components/headers/HomeNavbar";
+import OtherNavbar from "./components/headers/OtherNavbar";
+import Footer from "./components/footer";
 import "../css/app.css";
 import "../css/navbar.css";
+import "../css/footer.css";
 
 function App() {
   const location = useLocation();
--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
-export function OtherNavbar() {
+export default function OtherNavbar() {
   const authMember = null;
   return (
     <div className="other-navbar">
