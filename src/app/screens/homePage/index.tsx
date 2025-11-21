573534fb4e3ee97f084da08195e9a16459832bf5
Nick <javokhirru03@gmail.com>
2025-11-21 18:04:08 +0900

    feat: adding homePage related screen components

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -6,7 +6,6 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
-import "../../../css/home.css";
 
 export default function HomePage() {
   return (
